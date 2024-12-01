b98728e8084d34394d8c17d798219c30d68171e4
take44444 <take44444.general@gmail.com>
2024-12-01 15:17:11 +0900

    create project API

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,59 +1,47 @@
 import { ProjectModel } from "@/models/project.model";
 import { Injectable } from "@nestjs/common";
 import { GremlinService } from "./gremlin.service";
-
-function normalizeData(gremlinData: Map<string, any>) {
-  // Do this so that JSON.stringify works for maps
-  (Map.prototype as any).toJSON = function () {
-    return Object.fromEntries(this);
-  };
-  const mapStrippedData = JSON.parse(JSON.stringify(gremlinData));
-  // Undo it so that we don't permanently pollute globals
-  (Map.prototype as any).toJSON = undefined;
-
-  return mapStrippedData;
-}
+import * as gremlin from 'gremlin';
+import { CreateProjectInput } from "@/dto/project.dto";
 
 @Injectable()
 export class ProjectService {
-  constructor(private gremlin: GremlinService) {}
+  constructor(private gremlinService: GremlinService) {}
 
-  async project() {
-    await this.gremlin.g.addV('project').property('name', 'clar').next();
-    const project = normalizeData((await this.gremlin.g.V().hasLabel('project').next()).value);
-    await this.gremlin.g.V().drop().next();
+  async findOne(id: number) {
+    const project = (await this.gremlinService.g.V(id).elementMap().next()).value;
+    if (project === null) return;
     const ret: ProjectModel = {
-      id: project.id,
-      name: project.properties.name[0].value,
-      members: [
-        {
-          user: {
-            id: 1,
-            name: "Takeshi Masumoto"
-          },
-          roles: [
-            {
-              id: 2,
-              name: "Administrator",
-              project: {
-                id: project.id,
-                name: project.properties.name[0].value,
-              }
-            }
-          ],
-          tasks: [
-            {
-              id: 3,
-              name: "create gremlin service",
-              process: {
-                id: 4,
-                name: "first step",
-              }
-            }
-          ]
-        }
-      ]
+      id: project.get(gremlin.process.t.id),
+      name: project.get("name"),
     };
     return ret;
+  }
+
+  async findAll() {
+    const projects: ProjectModel[] = new Array();
+    for await (const project of this.gremlinService.g.V().hasLabel('project').elementMap()) {
+      projects.push({
+        id: project.get(gremlin.process.t.id),
+        name: project.get("name"),
+      })
+    }
+    return projects;
+  }
+
+  async create(createProjectInput: CreateProjectInput) {
+    const project = (await this.gremlinService.g.addV('project').property('name', createProjectInput.name).elementMap().next()).value;
+    const ret: ProjectModel = {
+      id: project.get(gremlin.process.t.id),
+      name: project.get("name"),
+    };
+    return ret;
+  }
+
+  async delete(id: number) {
+    const project = this.findOne(id);
+    if (project === null) return;
+    await this.gremlinService.g.V(id).drop().next();
+    return project;
   }
 }
