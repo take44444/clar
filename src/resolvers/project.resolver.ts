f1ab5837549388ff392c5c6feca6e79ca216c6be
take44444 <take44444.general@gmail.com>
2024-12-03 01:13:14 +0900

    [wip] native supporting GraphQL with Gremlin by DFS GraphQL query for project model

--- src/resolvers/project.resolver.ts
+++ src/resolvers/project.resolver.ts
@@ -1,31 +1,46 @@
 import { CreateProjectInput } from "@/dto/project.dto";
 import { ProjectModel } from "@/models/project.model";
 import { ProjectService } from "@/services/project.service";
+import { FieldMap } from "@jenyus-org/graphql-utils";
+import { FieldMap as InfoMap } from "@jenyus-org/nestjs-graphql-utils";
 import { Inject } from "@nestjs/common";
 import { Args, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
 
 @Resolver((of) => ProjectModel)
 export class ProjectResolver {
-  constructor(@Inject(ProjectService) private projectService: ProjectService) {}
+  constructor(
+    @Inject(ProjectService) private projectService: ProjectService
+  ) {}
 
   @Query((returns) => ProjectModel, { nullable: true })
-  async project(@Args("id", { type: () => Int }) id: number) {
-    return this.projectService.findOne(id);
+  async project(
+    @Args("id", { type: () => Int }) id: number,
+    @InfoMap() fieldMap: FieldMap
+  ) {
+    return this.projectService.findOne(id, fieldMap);
   }
 
-  @Query((returns) => [ProjectModel])
-  async projects() {
-    return this.projectService.findAll();
-  }
+  // @Query((returns) => [ProjectModel])
+  // async projects(
+  //   @InfoMap() fieldMap: FieldMap
+  // ) {
+  //   return this.projectService.findAll(fieldMap);
+  // }
 
-  @Mutation((returns) => ProjectModel)
-  async createProject(@Args("project") project: CreateProjectInput) {
-    return await this.projectService.create(project);
-  }
+  // @Mutation((returns) => ProjectModel)
+  // async createProject(
+  //   @Args("project") project: CreateProjectInput,
+  //   @InfoMap() fieldMap: FieldMap
+  // ) {
+  //   return await this.projectService.create(project, fieldMap);
+  // }
 
-  @Mutation((returns) => ProjectModel, { nullable: true })
-  async deleteProject(@Args("id", { type: () => Int }) id: number) {
-    return await this.projectService.delete(id);
-  }
+  // @Mutation((returns) => ProjectModel, { nullable: true })
+  // async deleteProject(
+  //   @Args("id", { type: () => Int }) id: number,
+  //   @InfoMap() fieldMap: FieldMap
+  // ) {
+  //   return await this.projectService.delete(id, fieldMap);
+  // }
 }
 
