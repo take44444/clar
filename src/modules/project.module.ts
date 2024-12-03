4ddcbb400a84e28cd1abb7899b8f948ecdc53a66
take44444 <take44444.general@gmail.com>
2024-12-04 01:38:39 +0900

    support project query

--- src/modules/project.module.ts
+++ src/modules/project.module.ts
@@ -1,11 +1,12 @@
-import { ProjectResolver } from "@/resolvers/project.resolver";
-import { GremlinService } from "@/services/gremlin.service";
-import { ProjectService } from "@/services/project.service";
 import { Module } from "@nestjs/common";
+import { GremlinRepository } from "@/repository/repository";
+import { ProjectRepository } from "@/repository/project.repository";
+import { ProjectService } from "@/services/project.service";
+import { ProjectResolver } from "@/resolvers/project.resolver";
 
 @Module({
   imports: [],
-  providers: [ProjectService, ProjectResolver, GremlinService],
+  providers: [ProjectService, ProjectResolver, ProjectRepository, GremlinRepository],
   exports: [ProjectService],
 })
 export class ProjectModule {}
